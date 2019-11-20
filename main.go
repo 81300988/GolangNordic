301ab56c5773ced90522f89b34f150d1c7a4ef6c
Hoàng Hải Hà <hhh@netcompany.com>
2019-11-20 14:53:10 +0700

    First commit

---
+++ main.go
@@ -0,0 +1,26 @@
+// Hoang Hai Ha
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Student struct {
+	FirstName string `json:"first_name" bson:"full_name" validate:"required"`
+	LastName  string `json:"last_name" bson:"full_name" validate:"required"`
+	Age       int
+	ClassName string `json:"class_name"`
+}
+
+func main() {
+	dataJson := `[
+		{"first_name": "Victor", "last_name": "Nguyen", "age": 100, "class_name":"golang"},
+		{"first_name": "Anh", "last_name": "Dinh", "age":200, "class_name":"golang"}
+		]`
+
+	var arrStudent []Student
+	_ = json.Unmarshal([]byte(dataJson), &arrStudent)
+	fmt.Println(arrStudent)
+
+}
